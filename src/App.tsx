3613dced0ba7358eb5bed99bcb2c8451a245099a
volx <97158976+vo1x@users.noreply.github.com>
2025-01-12 13:30:22 +0545

    init: batman

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import { Loader2, Search } from "lucide-react";
+import Results from "./components/Results";
+import { ChangeEvent, useState } from "react";
+
+function App() {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [searchResults, setSearchResults] = useState([]);
+  const fetchSearchResults = async (
+    query: string,
+    repacks: string[] = ["steamrip", "fitgirl"]
+  ) => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `http://localhost:3000/search?q=${encodeURIComponent(
+          query
+        )}&repacks=${encodeURIComponent(repacks.join(","))}`
+      );
+
+      const data = await response.json();
+      if (data.success) {
+        setSearchResults(data.results);
+        setLoading(false);
+
+        return;
+      }
+    } catch (error: any) {
+      setLoading(false);
+      console.error(error.message);
+      return;
+    }
+  };
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <>
+      <div className="flex flex-col h-screen w-screen items-center space-y-8 justify-center p-8">
+        <div className="flex w-full max-w-2xl flex-col items-center space-y-8">
+          <h1 className="text-5xl font-bold text-slate-300">GamesDexer</h1>
+
+          <div className="flex w-96 items-center gap-2 rounded-full border border-slate-700 bg-[#161B2E] p-2 px-3">
+            <button
+              onClick={() => fetchSearchResults(inputValue)}
+              className="rounded-full bg-[#445173] p-2 text-slate-400 transition-all duration-200 hover:text-slate-100"
+            >
+              {loading ? <Loader2 className="animate-spin" /> : <Search />}
+            </button>
+
+            <input
+              type="text"
+              onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                setInputValue(event.target.value)
+              }
+              placeholder="Search any game"
+              className="w-full rounded-full bg-[#0F131F] p-2 px-4 text-lg outline-none"
+            />
+          </div>
+        </div>
+
+        {searchResults?.length > 0 && <Results results={searchResults} />}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Results.tsx
@@ -0,0 +1,45 @@
+import { SquareArrowOutUpRight, Loader2 } from "lucide-react";
+import { useState } from "react";
+
+type SearchResultsProps = {
+  results: any[];
+};
+
+const ResultCard: React.FC<{ result: any }> = ({ result }) => {
+  return (
+    <div className="bg-[#161B2E] border rounded-lg border-slate-700 p-4 flex items-center justify-between">
+      <div className="overflow-hidden">
+        <h1 className="truncate max-w-lg text-lg font-semibold">
+          {result.name}
+        </h1>
+
+        <div className="flex items-center gap-4">
+          <span className=" text-slate-400">{result.version || "Unkown"}</span>
+          <a
+            className=" text-slate-400"
+            href={`https://${result.source}`}
+            target="_blank"
+          >
+            {result.repack}
+          </a>
+        </div>
+      </div>
+      <a href={`${result.url}`} target="_blank">
+        <SquareArrowOutUpRight className="text-slate-400 hover:text-slate-300" />
+      </a>
+    </div>
+  );
+};
+
+const Results: React.FC<SearchResultsProps> = ({ results }) => {
+
+  return (
+    <div className="custom-scrollbar relative pr-2 flex flex-col space-y-4 max-h-1/2 overflow-y-scroll max-w-3xl w-full">
+      {results.map((result: any) => (
+        <ResultCard result={result} />
+      ))}
+    </div>
+  );
+};
+
+export default Results;
